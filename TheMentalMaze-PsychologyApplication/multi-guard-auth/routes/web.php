eeba990f42ab63998367ea2226f0b19dc604c919
Hammad Ahmed <101525413+hammad-ahmed-01@users.noreply.github.com>
2023-05-14 22:46:30 +0500

    Add files via upload

---
+++ TheMentalMaze-PsychologyApplication/multi-guard-auth/routes/web.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\ResourceInsertController;
+use App\Http\Controllers\GroupsAndResourceViewController;
+use App\Http\Controllers\QuestionareController;
+use App\Http\Controllers\GroupDataInsertController;
+use App\Http\Controllers\CheckGroupsController;
+use App\Http\Controllers\PostQuestionController;
+use App\Http\Controllers\PostAnswerController;
+use App\Http\Controllers\CheckGroupExistanceController;
+// use App\Http\Middleware\CheckGroup;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// store resources from the helper
+Route::get('updated',[ResourceInsertController::class,'index'])->middleware('auth:helper');
+Route::post('resources',[ResourceInsertController::class,'store']);
+
+// store data for Support Groups from the helper
+Route::post('data-for-supportgroups',[GroupDataInsertController::class,'store'])->middleware('auth:helper');
+
+// view relevant resources
+Route::get('/profile',[GroupsAndResourceViewController::class,'index'])->middleware('auth');
+
+// store answers from the user
+Route::get('stored',[QuestionareController::class,'index'])->middleware('auth');
+Route::post('store-answers',[QuestionareController::class,'store'])->middleware('auth');
+
+// support groups
+Route::get('/support-group/{name}', [CheckGroupsController::class, 'show'])->middleware('auth')->name('show');
+Route::get('/support-group/admin-view/{name}', [CheckGroupsController::class, 'show'])->middleware('auth:helper')->name('show');
+
+// store questions from the user in group
+Route::post('store-question',[PostQuestionController::class,'store'])->middleware('auth');
+
+// store answers from the user in group
+Route::post('store-answer',[PostAnswerController::class,'store'])->middleware('auth');
+
+
+Auth::routes();
+
+Route::get('/helper/login',[LoginController::class,'showHelperLoginForm'])->name('helper.login-view');
+Route::post('/helper/login',[LoginController::class,'helperLogin'])->name('helper.login');
+
+Route::get('/helper/register',[RegisterController::class,'showHelperRegisterForm'])->name('helper.register-view');
+Route::post('/helper/register',[RegisterController::class,'createHelper'])->name('helper.register');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/helper/dashboard', [CheckGroupExistanceController::class, 'index'])->middleware('auth:helper');
